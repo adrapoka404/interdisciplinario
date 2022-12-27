20438b18d2af942888f15323be5fd1f5ca46efca
Alam <jazielhernandez275@gmail.com>
2022-12-27 16:43:25 -0600

    'creacionrutasnoticias'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\EditoresController;
+use App\Http\Controllers\Admin\NoticiasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,4 +29,5 @@ Route::middleware([
     })->name('dashboard');
 
     Route::resource('editores', EditoresController::class)->names('editores');
+    Route::resource('noticias', NoticiasController::class)->names('noticias');
 });
