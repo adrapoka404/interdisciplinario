82b40906f5f30117406d1984a5d4be9274e0eef4
Alam <jazielhernandez275@gmail.com>
2022-12-29 11:06:30 -0600

    mensaje

--- app/Http/Controllers/Admin/NoticiasController.php
+++ app/Http/Controllers/Admin/NoticiasController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Noty;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,7 @@ class NoticiasController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
